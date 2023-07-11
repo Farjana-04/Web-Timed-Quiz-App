dc0e6ce0660d2f617d4e669fcaa095c66c76fa13
Farjana Akhter <farjana.akhter085@gmail.com>
2023-07-11 10:24:00 -0500

    randomly suffuled array, set the counsown timer in script.js file, and modified the id in index.html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var startButton = document.getElementById("start-btn");
 var nextButton = document.getElementById("next-btn");
 var questionContainerEl = document.getElementById("question-container");
 var startContainerEl = document.getElementById("start-container");
-var questionEl = document.getElementById("question");
+var titleEl = document.getElementById("titleQuestions");
+// var questionEl = document.getElementById("question"); //titleEl "title"
 var answerButtonsEl = document.getElementById("answer-buttons");
 var checkAnswerEl = document.getElementById("check-answer");
 var viewHighScores = document.getElementById("highscores-link");
@@ -16,6 +17,7 @@ var scores = JSON.parse(localStorage.getItem("scores")) || [];
 var timeLeft = 75;
 var timerID;
 var timerEl = document.getElementById("timer");
+//randomly shuffled array, and genarate random index number
 var shuffledQuestions, currentQuestionIndex;
 
 // This is the question functions that contain questions and the answers. They are in multidimensional array with inner array elements
@@ -53,6 +55,79 @@ var questions = [
       answer: 'console.log',
     },
   ];
+
+  // Start button trigger the first question and next button to display
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+});
+// Start Quiz, hide the startbutton after clicked
+function startGame() {
+  timerID = setInterval(timeTick, 1000);
+  startContainerEl.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerEl.classList.remove("hide");
+
+  // Timer will start as soon as start button is clicked
+  timeTick();
+  setNextQuestion();
+};
+// Go to next question, and genarate randomly shuffled array
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+// Used the countdown timer 
+function timeTick() {
+  timeLeft--;
+  timerEl.textContent = "Time: " + timeLeft;
+  if (timeLeft <= 0) {
+      saveScore();
+  }
+}
+// Created a title element, display questions and click the answer
+function showQuestion(question) {
+  titleEl.innerText = question.title
+  question.choices.forEach(choice => {
+      var button = document.createElement("button")
+      button.innerText = choice
+      button.classList.add("btn")
+      if (choice.correct) {
+          button.dataset.correct = choice.correct
+      }
+      button.addEventListener("click", selectAnswer)
+      answerButtonsEl.appendChild(button)
+  })
+};
+// Reset state function, and initialize the state with variables
+
+function resetState() {
+   nextButton.classList.add("hide")
+  checkAnswerEl.classList.add("hide")
+  while (answerButtonsEl.firstChild) {
+      answerButtonsEl.removeChild
+          (answerButtonsEl.firstChild)
+  }
+};
+
+// if (choice.correct) {
+//   button.dataset.correct = choice.correct
+// }
+// button.addEventListener("click", selectAnswer)
+// answerButtonsEl.appendChild(button)
+// })
+// };
+
+
+
+
+
+
+
+
+
 
 
 
